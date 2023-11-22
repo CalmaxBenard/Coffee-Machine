3cfedfc6bd60e0ec504460218d5d3bee93d353bd
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-22 08:58:08 +0300

    model coffee making machine

---
+++ coffee_maker.py
@@ -0,0 +1,9 @@
+class CoffeeMaker:
+    """Models machine that make coffee"""
+    def __init__(self):
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
+
