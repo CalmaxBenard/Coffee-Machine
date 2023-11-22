673c2ed58e79e3fc547502d63e103bf462c7bb36
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-22 09:09:34 +0300

    check if ingredients are sufficient and make coffee

--- coffee_maker.py
+++ coffee_maker.py
@@ -12,3 +12,16 @@ class CoffeeMaker:
         print(f"Water: {self.resources['water']}ml")
         print(f"Milk: {self.resources['milk']}ml")
         print(f"Coffee: {self.resources['coffee']}g")
+
+    def is_sufficient(self, drink):
+        can_make = True
+        for item in drink.ingredients:
+            if drink.ingredients[item] > self.resources[item]:
+                print(f"Sorry there is not enough {item}.")
+                can_make = False
+        return can_make
+
+    def make_coffee(self, order):
+        for item in order.ingredients:
+            self.resources[item] -= order.ingredients[item]
+        print(f"Here is your {order.name}. Enjoy!")
