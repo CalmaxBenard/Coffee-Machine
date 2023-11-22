e332ab750e9ce91d50d9cb7790c522189f4f046f
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-22 08:52:36 +0300

    search for a particular item by name in the menu

--- menu.py
+++ menu.py
@@ -26,4 +26,9 @@ class Menu:
             options += f"{item.name}/"
         return options
 
-
+    def find_drink(self, order_name):
+        """Search the menu for a particular drink by name, returns the item if it exists, otherwise none"""
+        for item in self.menu:
+            if item.name == order_name:
+                return item
+            print("Sorry that item is not available")
