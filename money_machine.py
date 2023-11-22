1abc4b9356b5318c297913cd84be0aa012fb2fe1
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-22 09:22:33 +0300

    model the money machine

---
+++ money_machine.py
@@ -0,0 +1,17 @@
+class MoneyMachine:
+    CURRENCY = "$"
+
+    COIN_VALUES = {
+        "quarters": 0.25,
+        "dimes": 0.10,
+        "nickles": 0.05,
+        "pennies": 0.01
+    }
+
+    def __init__(self):
+        self.profit = 0
+        self.money_received = 0
+
+    def report(self):
+        """Prints the current profit"""
+        print(f"Money: {self.CURRENCY}{self.profit}")
